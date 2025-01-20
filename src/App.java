d84db21d05768b26134b83e7c31c227b4e2cc735
Renato <andresuyaguari200567@gmail.com>
2025-01-20 12:33:37 -0500

    grafos  DFS-BFS

--- src/App.java
+++ src/App.java
@@ -1,3 +1,4 @@
+
 import java.util.List;
 import main.Ejercicio_01_insert.InsertBTTest;
 import main.Ejercicio_02_invert.InvertBinaryTree;
@@ -11,6 +12,7 @@ import main.materia.controllers.ArbolRE;
 import main.materia.controllers.Graph;
 
 public class App {
+
     public static void main(String[] args) throws Exception {
         //runArbolBinario();
         //runArbol2();
@@ -21,118 +23,132 @@ public class App {
         //runEjercicio4();
         runGraph();
         //INCIAR GRAFOS
-        }
-        private static void runGraph(){
-            Graph grafo = new Graph();
-            NodeG a = grafo.addNode(5);
-            NodeG b = grafo.addNode(7);
-            NodeG c = grafo.addNode(9);
-            NodeG d = grafo.addNode(11);
-            NodeG e = grafo.addNode(3);
-            grafo.addEdge(a, b);
-            grafo.addEdge(a, c);
-            grafo.addEdge(a, e);
-            grafo.addEdge(e, d);
-            grafo.addEdge(c, e);
-            grafo.addEdge(b, c);
-            grafo.printGraph();
-        }
-        
-        private static void runEjercicio4(){
-            Depth tree = new  Depth();
-            tree.insert(4);
-            tree.insert(2);
-            tree.insert(7);
-            tree.printTree();
-            int var = tree.calculateDepth();
-            System.out.println(" Profundidad: " + var);
-            
-        }
-        private static void runEjercicio2(){
-            InvertBinaryTree invert = new InvertBinaryTree();
-            invert.insert(4);
-            invert.insert(2);
-            invert.insert(7);
-            invert.insert(1);
-            invert.insert(3);
-            invert.insert(6);
-            invert.insert(9);
-            System.out.println("Arbol : ");
-            invert.printTree();
-            invert.invertTree();
-            System.out.println("Arbol Invertido: ");
-            invert.printTree();
-        }
-            private static void runEjercicio1(){
-                InsertBTTest insertarA = new InsertBTTest();
-                insertarA.insert(5);
-                insertarA.insert(3);
-                insertarA.insert(7);
-                insertarA.insert(2);
-                insertarA.insert(4);
-                insertarA.insert(6);
-                insertarA.insert(8);
-                insertarA.printTree();
-                
-            }
-            private static void runArbolBinario() {
-                ArbolBinario arbolB = new ArbolBinario();
-                int[] valores = {40,20,60,10,30,50,70,5,15,55};
-                for (int i = 0; i < valores.length; i++) {
-                    arbolB.insert(valores[i]);
-                }
-                arbolB.printTree();
-            }
-            private static void runEjercicio3(){
-                ArbolBinario arbolB = new ArbolBinario();
-                int[] valores = {4, 2, 7, 1, 3, 6, 9};
-                for (int valor : valores) {
-                arbolB.insert(valor);
-                }
+    }
 
-                System.out.println("Árbol binario:");
-                arbolB.printTree();
+    private static void runGraph() {
 
+        Graph grafo = new Graph();
+        NodeG h = grafo.addNode(0);
+        NodeG a = grafo.addNode(1);
+        NodeG b = grafo.addNode(2);
+        NodeG c = grafo.addNode(3);
+        NodeG d = grafo.addNode(4);
+        NodeG e = grafo.addNode(5);
 
-                ListLevels levels = new ListLevels();
-                List<List<Node>> result = levels.ListLevels(arbolB.getRoot());
+        grafo.addEdgeDirect(h, c);
+        grafo.addEdgeDirect(a, h);
+        grafo.addEdgeDirect(h, e);
+        grafo.addEdgeDirect(c, b);
+        grafo.addEdgeDirect(c, d);
+        grafo.addEdgeDirect(b, a);
 
-                System.out.println("\nNiveles del árbol:");
-                for (int i = 0; i < result.size(); i++) {
-                List<Node> level = result.get(i);
-                System.out.print("Nivel " + i + ": ");
-                for (Node node : level) {
+        grafo.printGraph();
+
+        grafo.getDFS(h);
+
+        grafo.getBFS(h);
+    }
+
+    private static void runEjercicio4() {
+        Depth tree = new Depth();
+        tree.insert(4);
+        tree.insert(2);
+        tree.insert(7);
+        tree.printTree();
+        int var = tree.calculateDepth();
+        System.out.println(" Profundidad: " + var);
+
+    }
+
+    private static void runEjercicio2() {
+        InvertBinaryTree invert = new InvertBinaryTree();
+        invert.insert(4);
+        invert.insert(2);
+        invert.insert(7);
+        invert.insert(1);
+        invert.insert(3);
+        invert.insert(6);
+        invert.insert(9);
+        System.out.println("Arbol : ");
+        invert.printTree();
+        invert.invertTree();
+        System.out.println("Arbol Invertido: ");
+        invert.printTree();
+    }
+
+    private static void runEjercicio1() {
+        InsertBTTest insertarA = new InsertBTTest();
+        insertarA.insert(5);
+        insertarA.insert(3);
+        insertarA.insert(7);
+        insertarA.insert(2);
+        insertarA.insert(4);
+        insertarA.insert(6);
+        insertarA.insert(8);
+        insertarA.printTree();
+
+    }
+
+    private static void runArbolBinario() {
+        ArbolBinario arbolB = new ArbolBinario();
+        int[] valores = {40, 20, 60, 10, 30, 50, 70, 5, 15, 55};
+        for (int i = 0; i < valores.length; i++) {
+            arbolB.insert(valores[i]);
+        }
+        arbolB.printTree();
+    }
+
+    private static void runEjercicio3() {
+        ArbolBinario arbolB = new ArbolBinario();
+        int[] valores = {4, 2, 7, 1, 3, 6, 9};
+        for (int valor : valores) {
+            arbolB.insert(valor);
+        }
+
+        System.out.println("Árbol binario:");
+        arbolB.printTree();
+
+        ListLevels levels = new ListLevels();
+        List<List<Node>> result = levels.ListLevels(arbolB.getRoot());
+
+        System.out.println("\nNiveles del árbol:");
+        for (int i = 0; i < result.size(); i++) {
+            List<Node> level = result.get(i);
+            System.out.print("Nivel " + i + ": ");
+            for (Node node : level) {
                 System.out.print(node.getValue() + " ");
-                }
-                System.out.println();
-                }
             }
-            public static void runArbol2() {
+            System.out.println();
+        }
+    }
 
-                ArbolBinario arbolbinario = new ArbolBinario();
-                int[] valores = {40, 20, 60, 10, 30, 50, 70, 5, 15, 55};
-                for (int valor : valores) {
-                    arbolbinario.insert(valor);
-                }
-                arbolbinario.printTree();
-                ArbolRE arbolRecorridos = new ArbolRE();
-                System.out.println("\nRecorrido en Preorden (Iterativo):");
-                arbolRecorridos.preOrderIterativo(arbolbinario.getRoot());  
-                System.out.println("\nrecorrido en Preorde (recursivo): ");
-                arbolRecorridos.preOrderRecursivo(arbolbinario.getRoot());
-                System.out.println("\nrecorrido en Post order (Recursivo)");
-                arbolRecorridos.postOrderRecursivo(arbolbinario.getRoot());
-                System.out.println("\nrecorrido en in Order (recursivo)");
-                arbolRecorridos.inOrderRecursivo(arbolbinario.getRoot());
-                
-            }
-                private static void runArbolAvl() {
-                int [] values = {10,20,15,24,9,8,21,23,50,25};   
-                ArbolAVL arbolavl = new ArbolAVL();
-                for(int num : values){
-                    arbolavl.insert(num);
-                }
-                     arbolavl.printTree();
+    public static void runArbol2() {
+
+        ArbolBinario arbolbinario = new ArbolBinario();
+        int[] valores = {40, 20, 60, 10, 30, 50, 70, 5, 15, 55};
+        for (int valor : valores) {
+            arbolbinario.insert(valor);
+        }
+        arbolbinario.printTree();
+        ArbolRE arbolRecorridos = new ArbolRE();
+        System.out.println("\nRecorrido en Preorden (Iterativo):");
+        arbolRecorridos.preOrderIterativo(arbolbinario.getRoot());
+        System.out.println("\nrecorrido en Preorde (recursivo): ");
+        arbolRecorridos.preOrderRecursivo(arbolbinario.getRoot());
+        System.out.println("\nrecorrido en Post order (Recursivo)");
+        arbolRecorridos.postOrderRecursivo(arbolbinario.getRoot());
+        System.out.println("\nrecorrido en in Order (recursivo)");
+        arbolRecorridos.inOrderRecursivo(arbolbinario.getRoot());
+
+    }
+
+    private static void runArbolAvl() {
+        int[] values = {10, 20, 15, 24, 9, 8, 21, 23, 50, 25};
+        ArbolAVL arbolavl = new ArbolAVL();
+        for (int num : values) {
+            arbolavl.insert(num);
+        }
+        arbolavl.printTree();
 
     }
 }
--- src/main/materia/controllers/Graph.java
+++ src/main/materia/controllers/Graph.java
@@ -1,31 +1,76 @@
 package main.materia.controllers;
 
 import java.util.*;
-
 import main.materia.Models.NodeG;
 
 public class Graph {
+
     private List<NodeG> nodes;
-    public Graph (){
+
+    public Graph() {
         this.nodes = new ArrayList<>();
     }
-    public NodeG addNode(int value){
+
+    public NodeG addNode(int value) {
         NodeG newNode = new NodeG(value);
         nodes.add(newNode);
         return newNode;
     }
-    public void addEdge(NodeG src, NodeG dest){
+
+    public void addEdge(NodeG src, NodeG dest) {
         src.addNeightbor(dest);
         dest.addNeightbor(src);
-    
+
     }
-    public void printGraph(){
-        for (NodeG ptr: nodes) {
+
+    public void printGraph() {
+        for (NodeG ptr : nodes) {
             System.out.println(" Vertice " + ptr.getValue() + ": ");
             for (NodeG nei : ptr.getNeighbors()) {
-                System.out.println(nei.getValue() +" - ");
-                System.out.println();
+                System.out.println(" ->  " + nei.getValue());
+
             }
         }
+    }
+
+    public void addEdgeDirect(NodeG src, NodeG dest) {
+        src.addNeightbor(dest);
+    }
+
+    public void getDFS(NodeG start) {
+        Set<NodeG> visitados = new HashSet<>();
+        System.out.println("DPS desde el nodo  " + start.getValue() + " : ");
+        getDFSDirect(start, visitados);
+    }
+
+    private void getDFSDirect(NodeG node, Set<NodeG> visitados) {
+        if (visitados.contains(node)) {
+            return;
+        }
+        System.out.print(node.getValue() + " ");
+        visitados.add(node);
+        for (NodeG neigh : node.getNeighbors()) {
+            getDFSDirect(neigh, visitados);
+        }
+    }
+
+    public void getBFS(NodeG start) {
+        Set<NodeG> visitados = new HashSet<>();
+        Queue<NodeG> cola = new LinkedList<>();
+        System.out.println();
+        System.out.println("BFS desde el nodo  " + start.getValue() + " : " );
+        cola.add(start);
+        visitados.add(start);
+        while (!cola.isEmpty()) {
+            NodeG actual = cola.poll();
+            System.out.print(actual.getValue() + " ");
+            for (NodeG nei : actual.getNeighbors()) {
+                if (!visitados.contains(nei)) {
+                    visitados.add(nei);
+                    cola.add(nei);
+                }
+            }
+        }
+
     }
 }
